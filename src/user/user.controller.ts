4bfd6004d7d8c892b33a3fcfb1c7ecc7b07b582b
Artyom Tratsiuk <inqui05@gmail.com>
2022-07-26 12:52:24 +0300

    fix: move throwing errors from service to controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,24 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AutoSuggestUserInfoDto } from './dto/autoSuggestUserInfo.dto';
 import { NewUserDto } from './dto/new-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
-const PHRASE = 'There is not user with id=';
+const WRONG_ID = 'There is not user with id=';
+const USER_MUST_BE_UNIQUE = 'The user\'s login must be unique';
 
-@Controller('v1/user')
+@Controller('v1/users')
 export class UserController {
   constructor(private users: UserService) {}
 
   @Get(':id')
   getUserById(@Param('id') id: string): UserDto {
-    return this.users.getUserById(id);
+    const result = this.users.getUserById(id);
+    if (!result) {
+      throw new HttpException(`${WRONG_ID}${id}`, HttpStatus.NOT_FOUND);
+    }
+    return result;
   }
 
   @Get()
@@ -28,19 +33,30 @@ export class UserController {
   @Post()
   @UsePipes(ValidationPipe)
   createUser(@Body() userInfo: NewUserDto): UserDto {
-    return this.users.createUser(userInfo);
+    const result = this.users.createUser(userInfo);
+    if (!result) {
+      throw new HttpException(USER_MUST_BE_UNIQUE, HttpStatus.BAD_REQUEST);
+    }
+    return result;
   }
 
   @Put(':id')
   @UsePipes(ValidationPipe)
   @HttpCode(HttpStatus.ACCEPTED)
   updateUser(@Param('id') id: string, @Body() userInfo: UpdateUserDto): UserDto {
-    return this.users.updateUser(id, userInfo);
+    const result = this.users.updateUser(id, userInfo);
+    if (!result) {
+      throw new HttpException(USER_MUST_BE_UNIQUE, HttpStatus.BAD_REQUEST);
+    }
+    return result;
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   removeUserById(@Param('id') id: string): void {
-    this.users.removeUserById(id);
+    const result = this.users.removeUserById(id);
+    if (!result) {
+      throw new HttpException(`${WRONG_ID}${id}`, HttpStatus.NOT_FOUND);
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,32 +1,26 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AutoSuggestUserInfoDto } from './dto/autoSuggestUserInfo.dto';
 import { NewUserDto } from './dto/new-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserDto } from './dto/user.dto';
-
-const PHRASE = 'There is not user with id=';
 
 @Injectable()
 export class UserService {
   public users: UserDto[] = [];
 
   getUsers(): UserDto[] {
-    return this.users.filter((user: UserDto): boolean => user.isDeleted === false);
+    return this.users.filter((user: UserDto): boolean => !user.isDeleted);
   }
 
-  getUserById(id: string): UserDto {
-    const result = this.users.find((user: UserDto): boolean => user.id === id && user.isDeleted === false);
-    if (!result) {
-      throw new HttpException(`${PHRASE}${id}`, HttpStatus.NOT_FOUND);
-    }
-    return result;
+  getUserById(id: string): UserDto | undefined {
+    return this.users.find((user: UserDto): boolean => user.id === id && !user.isDeleted);
   }
 
   getUsersWithParams(params: AutoSuggestUserInfoDto): UserDto[] {
     if (params.loginSubstring) {
       const users = this.users.filter((user: UserDto) => {
         if (params.loginSubstring) {
-          return user.login.includes(params.loginSubstring) && user.isDeleted === false;
+          return user.login.includes(params.loginSubstring) && !user.isDeleted;
         }
       });
 
@@ -37,11 +31,11 @@ export class UserService {
     return this.trimArray(users, params.limit);
   }
 
-  createUser(userInfo: NewUserDto): UserDto {
-    const checkLogin = this.users.filter((user: UserDto): boolean => user.login === userInfo.login);
+  createUser(userInfo: NewUserDto): UserDto | undefined {
+    const howManyLogins = this.users.filter((user: UserDto): boolean => user.login === userInfo.login);
 
-    if (checkLogin.length) {
-      throw new HttpException("The user's login must be unique", HttpStatus.BAD_REQUEST);
+    if (howManyLogins.length) {
+      return;
     } else {
       const newUser: UserDto = { ...userInfo, id: this.createId(), isDeleted: false };
       this.users.push(newUser);
@@ -49,30 +43,28 @@ export class UserService {
     }
   }
 
-  updateUser(id: string, newInfo: UpdateUserDto): UserDto {
-    const checkLogin = this.users.filter((user: UserDto): boolean => user.login === newInfo.login);
-    if (checkLogin.length > 1 || (checkLogin.length && checkLogin[0].id !== id)) {
-      throw new HttpException("The user's login must be unique", HttpStatus.BAD_REQUEST);
+  updateUser(id: string, newUserInfo: UpdateUserDto): UserDto | undefined {
+    const howManyLogins = this.users.filter((user: UserDto): boolean => user.login === newUserInfo.login);
+    if (howManyLogins.length > 1 || (howManyLogins.length && howManyLogins[0].id !== id)) {
+      return;
     }
 
     const userIndex = this.users.findIndex((user: UserDto): boolean => user.id === id);
     if (userIndex >= 0 && !this.users[userIndex].isDeleted) {
-      this.users[userIndex] = { ...this.users[userIndex], ...newInfo };
-    } else {
-      throw new HttpException(PHRASE + id, HttpStatus.NOT_FOUND);
+      this.users[userIndex] = { ...this.users[userIndex], ...newUserInfo };
+    } else if (userIndex === -1) {
+      return this.createUser(newUserInfo);
     }
 
     return this.users[userIndex];
   }
 
-  removeUserById(id: string): UserDto {
+  removeUserById(id: string): UserDto | undefined {
     const user = this.users.find((user: UserDto): boolean => user.id === id && !user.isDeleted);
     if (user) {
       user.isDeleted = true;
-      return user;
-    } else {
-      throw new HttpException(`${PHRASE}${id}`, HttpStatus.NOT_FOUND);
     }
+    return user;
   }
 
   private createId(): string {
