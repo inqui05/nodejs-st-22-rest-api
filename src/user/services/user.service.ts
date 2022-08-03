1dbed0c307584aa33fb7d4ca624b415f0eb41ccb
Artsiom Tratsiuk <inqui05@gmail.com>
2022-08-04 01:27:50 +0300

    fix: move DB accesses to user-repository file

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,71 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { AutoSuggestUserInfoDto } from '../dto/autoSuggestUserInfo.dto';
 import { User } from '../models/user.model';
-import { InjectModel } from '@nestjs/sequelize';
-import { Op } from 'sequelize';
+import { UserRepository } from '../repository/users-repository.service';
 import { NewUserDto } from '../dto/new-user.dto';
 import { UserDto } from '../dto/user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
 
-const WRONG_ID = 'There is not user with id=';
-const USER_MUST_BE_UNIQUE = 'The user\'s login must be unique';
-
 @Injectable()
 export class UserService {
 
-  constructor(@InjectModel(User) private userModel: typeof User) {}
+  constructor(private UserRepository: UserRepository) {}
 
   async getUsers(): Promise<User[]> {
-    return await this.userModel.findAll({ where: { isDeleted: false }});
+    return this.UserRepository.getAll();
   }
 
   async getUserById(id: string): Promise<User | null> {
-    return await this.userModel.findOne({where: { [Op.and]: [{ id }, { isDeleted: false }] }});
+    return this.UserRepository.getOne(id);
   }
 
   async createUser(newUserInfo: NewUserDto): Promise<UserDto | null> {
-    const isLoginUnique = await this.checkIsLoginUnique(newUserInfo.login);
-    return isLoginUnique ? await this.userModel.create(newUserInfo) : null;
+    return this.UserRepository.create(newUserInfo);
   }
 
   async removeUserById(id: string): Promise<User | null> {
-    const user = await this.userModel.findOne({ where: { id }});
-    if (user) {
-      user.update({ isDeleted: true })
-    }
-    return user;
+    return this.UserRepository.remove(id);
   }
 
-  async updateUser(id: string, newUserInfo: UpdateUserDto): Promise<UserDto | string> {
-    const user = await this.userModel.findOne({ where: { id }});
-    const isLoginUnique = await this.checkIsLoginUnique(newUserInfo.login, id);
-
-    if (user && isLoginUnique) {
-      user.update({ 
-        login: newUserInfo.login,
-        password: newUserInfo.password,
-        age: newUserInfo.age,
-      });
-      return user;
-    } else if (!isLoginUnique) {
-      return USER_MUST_BE_UNIQUE;
-    } else {
-      return `${WRONG_ID}${id}`
-    }
+  async updateUser(id: string, newUserInfo: UpdateUserDto): Promise<UserDto | null> {
+    return this.UserRepository.update(id, newUserInfo);
   }
 
   async getUsersWithParams(params: AutoSuggestUserInfoDto): Promise<UserDto[]> {
-    return await this.userModel.findAll({ 
-      where: { login: {[Op.iLike]: `%${params.loginSubstring}%` }},
-      limit: params.limit
-    });
-  }
-
-  async checkIsLoginUnique(login: string, id?: string): Promise<boolean> {
-    const users = await this.userModel.findAll({ where: { login }});
-
-    return !users.length || (users.length === 1 && users[0].id === id) ?
-      true :
-      false;
+    return this.UserRepository.getSome(params);
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
+import { UserRepository } from './repository/users-repository.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserRepository],
 })
 export class UserModule {}
