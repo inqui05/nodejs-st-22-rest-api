28fbf675a0966a3a0be453dc6c9d731470ee1f33
Artsiom Tratsiuk <inqui05@gmail.com>
2022-08-09 14:21:05 +0300

    feat: add UserGroup table to implement many-to-many relationship between User and Group tables

--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -4,9 +4,11 @@ import { GroupController } from './controllers/group.controller';
 import { GroupRepository } from './repository/group-repository.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Group } from './models/group.model';
+import { User } from 'src/user/models/user.model';
+import { UserGroup } from 'src/user-group/models/user-group.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Group])],
+  imports: [SequelizeModule.forFeature([Group, User, UserGroup])],
   controllers: [GroupController],
   providers: [GroupService, GroupRepository],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,11 @@ import { User } from './models/user.model';
 import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
 import { UserRepository } from './repository/users-repository.service';
+import { Group } from 'src/group/models/group.model';
+import { UserGroup } from 'src/user-group/models/user-group.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [SequelizeModule.forFeature([User, Group, UserGroup])],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
