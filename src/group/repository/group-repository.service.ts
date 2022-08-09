25e5a0b38424738d5b70d228ed64aef9fd05c93a
Artsiom Tratsiuk <inqui05@gmail.com>
2022-08-09 17:23:47 +0300

    feat: add method which adding users to group by transaction

--- src/group/repository/group-repository.service.ts
+++ src/group/repository/group-repository.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Sequelize } from 'sequelize-typescript';
 import { CreateGroupDto } from '../dto/create-group.dto';
 import { UpdateGroupDto } from '../dto/update-group.dto';
 import { IGroup } from '../interfaces/group.interface';
@@ -8,7 +9,7 @@ import { Group } from '../models/group.model';
 @Injectable()
 export class GroupRepository {
 
-  constructor(@InjectModel(Group) private groupModel: typeof Group) {}
+  constructor(@InjectModel(Group) private groupModel: typeof Group, private sequelize: Sequelize) {}
 
   async create(createGroupDto: CreateGroupDto): Promise<IGroup | null> {
     const isLoginUnique = await this.checkIsNameUnique(createGroupDto.name);
@@ -30,6 +31,21 @@ export class GroupRepository {
 
   async remove(id: string): Promise<void> {
     this.groupModel.destroy({ where: { id } });
+  }
+
+  async addUsersToGroup(id: string, usersIds: string[]): Promise<void> {
+    await this.sequelize.transaction(async t => {
+      const transactionHost = { transaction: t };
+
+      const group: Group | null = await this.groupModel.findOne({
+        where: { id },
+        ...transactionHost,
+      });
+
+      if (group) {
+        await Promise.all(usersIds.map((usersId) => group.$add('users', usersId, transactionHost)));
+      }
+    });
   }
 
   async checkIsNameUnique(name: string, id?: string): Promise<boolean> {
