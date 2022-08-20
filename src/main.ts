2ab223fd11b5cd1f9e521fcb9f84def0563a2f18
Artsiom Tratsiuk <inqui05@gmail.com>
2022-08-20 14:24:56 +0300

    fix: move error handling from middleware to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
+const safeJsonStringify = require('safe-json-stringify');
+import { Logger } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+const logger = new Logger('HTTP');
+
+process.on('unhandledRejection', (reason, promise) => {
+  const promiseError = safeJsonStringify(promise, null, 2);
+  logger.error(`${reason}. Unhandled Rejection at: ${promiseError}`);
+  process.exit(1);
+});
+
+process.on('UncaughtException', (err) => {
+  logger.error(err);
+  process.exit(1);
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
